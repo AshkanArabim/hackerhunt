f42d6766d93423927082943b8747fd98adc9ebab
AshkanArabim <ashkan.arabim@gmail.com>
2025-03-10 20:19:29 -0600

    delete notes django app

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "notes",
     "rest_framework_simplejwt",
     "rest_framework",
     "rest_framework.authtoken",
--- backend/notes/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import ImageNote, TextNote, User
-
-# Register your models here.
-admin.site.register(ImageNote)
-admin.site.register(TextNote)
